2ca14001d749ca6ed5a6c8dc96664f6f75692595
Alec Keller <aleckeller@macbook-pro.myfiosgateway.com>
2024-02-27 21:00:37 -0500

    fix vote

--- db/migrations/functions.py
+++ db/migrations/functions.py
@@ -4,34 +4,33 @@ from alembic_utils.pg_trigger import PGTrigger
 update_leader_function_query = """
 RETURNS TRIGGER AS $$
 DECLARE
-   debate_id INT;
-   response_id INT;
+   incoming_debate_id INT;
+   incoming_response_id INT;
 BEGIN
     IF TG_OP = 'INSERT' THEN
-        response_id := NEW.response_id;
+        incoming_response_id := NEW.response_id;
     ELSIF TG_OP = 'DELETE' THEN
-        response_id := OLD.response_id;
+        incoming_response_id := OLD.response_id;
     END IF;
-   RAISE NOTICE 'Debug message: Variable value = %', debate_id;
 
-   SELECT rv.debate_id INTO debate_id
+   SELECT rv.debate_id INTO incoming_debate_id
    FROM responses_view AS rv
-   WHERE rv.id = response_id;
+   WHERE rv.id = incoming_response_id;
+
+   RAISE NOTICE 'Debug message: Variable value = %', incoming_debate_id;
 
    UPDATE debate AS d
    SET leader_id = (
-       SELECT
-           CASE
-               WHEN COUNT(*) > 1 THEN NULL
-               ELSE rv.created_by_id
-           END
-       FROM responses_view AS rv
-       WHERE d.id = rv.debate_id
-       GROUP BY rv.vote_difference, rv.created_by_id
-       ORDER BY rv.vote_difference DESC
-       LIMIT 1
+    SELECT
+        CASE 
+            WHEN COUNT(*) > 1 THEN NULL
+            ELSE MAX(rv.created_by_id::varchar)::uuid
+        END
+    FROM responses_view rv
+    WHERE rv.vote_difference = (SELECT MAX(rv.vote_difference) FROM responses_view rv WHERE rv.debate_id = incoming_debate_id)
+    AND rv.debate_id = incoming_debate_id
    )
-   WHERE d.id = debate_id;
+   WHERE d.id = incoming_debate_id;
    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
